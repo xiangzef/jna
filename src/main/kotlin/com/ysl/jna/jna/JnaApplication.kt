93129a3c77e5af66d5940509dda1ed0d50b2bce4
xiangzef <414395321@qq.com>
2018-06-19 19:22:59 +0800

    first commit

---
+++ src/main/kotlin/com/ysl/jna/jna/JnaApplication.kt
@@ -0,0 +1,11 @@
+package com.ysl.jna.jna
+
+import  org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JnaApplication
+
+fun main(args: Array<String>) {
+    runApplication<JnaApplication>(*args)
+}
---
+++ src/test/kotlin/com/ysl/jna/jna/JnaApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.ysl.jna.jna
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class JnaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
